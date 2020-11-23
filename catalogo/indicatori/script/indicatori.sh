a04cb9490bf6257f9b8f4a99a44dc3fe35b8ab62
Andrea Borruso <aborruso@gmail.com>
2020-11-23 08:28:08 +0100

    update

--- catalogo/indicatori/script/indicatori.sh
+++ catalogo/indicatori/script/indicatori.sh
@@ -12,7 +12,7 @@ folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mkdir -p "$folder"/rawdata
 mkdir -p "$folder"/processing
-mkdir -p "$folder"/../../
+mkdir -p "$folder"/../output
 
 rm "$folder"/rawdata/lista.jsonl
 
