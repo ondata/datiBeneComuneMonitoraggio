77dcb7e368cfe15498e3007cd0b53fc693cb51fb
Andrea Borruso <aborruso@gmail.com>
2020-11-26 00:05:28 +0100

    prima versione monitoraggio sorveglianza integrata

--- catalogo/sorveglianzaIntegrata/script/sorveglianzaIntegrata.sh
+++ catalogo/sorveglianzaIntegrata/script/sorveglianzaIntegrata.sh
@@ -14,11 +14,9 @@ mkdir -p "$folder"/rawdata
 mkdir -p "$folder"/processing
 mkdir -p "$folder"/../output
 
-rm "$folder"/rawdata/lista.jsonl
-
 nome="sorveglianzaIntegrata"
 
-URL="http://www.salute.gov.it/portale/nuovocoronavirus/dettaglioContenutiNuovoCoronavirus.jsp?lingua=italiano&id=5351&area=nuovoCoronavirus&menu=vuoto"
+URL="https://web.archive.org/web/timemap/?url=https%3A%2F%2Fwww.epicentro.iss.it%2Fcoronavirus%2Fbollettino%2F&matchType=prefix&collapse=urlkey&output=json&fl=original%2Cmimetype%2Ctimestamp%2Cendtimestamp%2Cgroupcount%2Cuniqcount&filter=!statuscode%3A%5B45%5D..&limit=100000"
 
 # leggi la risposta HTTP del sito
 code=$(curl -s -L -o /dev/null -w "%{http_code}" ''"$URL"'')
@@ -26,39 +24,19 @@ code=$(curl -s -L -o /dev/null -w "%{http_code}" ''"$URL"'')
 # se il sito è raggiungibile scarica  i dati
 if [ "$code" -eq 200 ]; then
 
-  # crea lista URL delle pagine di interesse
-  curl "http://www.salute.gov.it/portale/nuovocoronavirus/dettaglioContenutiNuovoCoronavirus.jsp?lingua=italiano&id=5351&area=nuovoCoronavirus&menu=vuoto" | scrape -be '//div[@class="container-fluid"]//a[contains(text(),"MONIT") or strong[contains(text(),"MONIT")]]' | xq -c '.html.body.a[]' | mlr --json rename -r '^(@|#)', then rename strong,text >>"$folder"/rawdata/listaURL.json
+  # scarica lista
+  curl $'https://web.archive.org/web/timemap/?url=https%3A%2F%2Fwww.epicentro.iss.it%2Fcoronavirus%2Fbollettino%2F&matchType=prefix&collapse=urlkey&output=json&fl=original%2Cmimetype%2Ctimestamp%2Cendtimestamp%2Cgroupcount%2Cuniqcount&filter=\u0021statuscode%3A%5B45%5D..&limit=100000' >"$folder"/rawdata/lista.json
+  jq <"$folder"/rawdata/lista.json -c '.[]' | sed -r 's/(\[|\])//g' >"$folder"/processing/lista.csv
 
-  mlr -I --json uniq -a "$folder"/rawdata/listaURL.json
-  mlr --j2n cut -f href "$folder"/rawdata/listaURL.json >"$folder"/rawdata/listaURL
+  mlr -I --csv cat "$folder"/processing/lista.csv
 
-  cat "$folder"/rawdata/listaURL >>"$folder"/processing/listaURL
+  mlr --csv filter '$mimetype=~"pdf" && tolower($original)=~"integrat"' \
+  then put '$url=sub($original,"^(.+)[?](.+)$","\1")' \
+  then put 'if (tolower($url)=~"appendix"){$tipo="appendice"}else{$tipo=""}' \
+  then put '$nomefile=sub($url,"^(.+/)(.+)$","\2")' \
+  then put '$data=sub($url,"^(.+COVID-19_)(.+2020)(.+)$","\2")' \
+  then cut -o -f nomefile,data,tipo,url "$folder"/processing/lista.csv >"$folder"/../output/"$nome".csv
 
-  mlr -I --csv -N uniq -a "$folder"/processing/listaURL
-
-  # per ogni URL estrai info sui PDF con i dati sugli indicatori
-  while read -r p; do
-    curl -kL "http://www.salute.gov.it$p" >"$folder"/rawdata/tmp_pagina.html
-    titoloPagina=$(scrape <"$folder"/rawdata/tmp_pagina.html -e '//title/text()' | tr '(\n|\r|\t)' ' ' | sed -r 's/ +/ /g')
-    scrape <"$folder"/rawdata/tmp_pagina.html -be '//a[contains(text(),"indica")]' | xq -c '.html.body.a| .|= .+ {url:"'"$p"'"}| .|= .+ {titoloPagina:"'"$titoloPagina"'"}' >>"$folder"/rawdata/listaFileRport.jsonl
-  done <"$folder"/processing/listaURL
-
-  # rimuovi righe che hanno restituito null
-  sed -i '/null/d' "$folder"/rawdata/listaFileRport.jsonl
-
-  mlr -I --json rename -r '(@|#)','' then filter -x -S '$href==""' then uniq -a "$folder"/rawdata/listaFileRport.jsonl
-
-  cat "$folder"/rawdata/listaFileRport.jsonl >"$folder"/processing/listaFileRport.jsonl
-
-  mlr -I --json uniq -a "$folder"/processing/listaFileRport.jsonl
-
-  # crea CSV con anagrafica lista PDF indicatori
-  mlr --j2c unsparsify \
-    then put '$href="http://www.salute.gov.it".$href;$url="http://www.salute.gov.it".$url' \
-    then cut -x -f onclick,title \
-    then rename href,hrefFile,text,titoloFile "$folder"/processing/listaFileRport.jsonl >"$folder"/../output/"$nome".csv
-
-  # crea file markdown anagrafica lista PDF indicatori
-  mlr --c2m put '$titoloFile="[".$titoloFile."](".$hrefFile.")"' then cut -f titoloFile,titoloPagina "$folder"/../output/"$nome".csv >"$folder"/../output/"$nome".md
+  mlr --c2m put '$nomefile="[".$nomefile."](".$url.")"' then cut -x -f url "$folder"/../output/"$nome".csv >"$folder"/../output/"$nome".md
 
 fi
