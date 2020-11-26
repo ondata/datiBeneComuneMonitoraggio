60af254e999f407adc0597384a2d3e29ac07aa72
Andrea Borruso <aborruso@gmail.com>
2020-11-26 10:56:11 +0100

    aggiunto commenti nello script

--- catalogo/sorveglianzaIntegrata/script/sorveglianzaIntegrata.sh
+++ catalogo/sorveglianzaIntegrata/script/sorveglianzaIntegrata.sh
@@ -26,8 +26,10 @@ if [ "$code" -eq 200 ]; then
   curl $'https://web.archive.org/web/timemap/?url=https%3A%2F%2Fwww.epicentro.iss.it%2Fcoronavirus%2Fbollettino%2F&matchType=prefix&collapse=urlkey&output=json&fl=original%2Cmimetype%2Ctimestamp%2Cendtimestamp%2Cgroupcount%2Cuniqcount&filter=\u0021statuscode%3A%5B45%5D..&limit=100000' >"$folder"/rawdata/lista.json
   jq <"$folder"/rawdata/lista.json -c '.[]' | sed -r 's/(\[|\])//g' >"$folder"/processing/lista.csv
 
+  # normalizza CSV
   mlr -I --csv cat "$folder"/processing/lista.csv
 
+  # estrai dal CSV url, tipo documento, nome file e data
   mlr --csv filter '$mimetype=~"pdf" && tolower($original)=~"integrat"' \
   then put '$url=sub($original,"^(.+)[?](.+)$","\1")' \
   then put 'if (tolower($url)=~"appendix"){$tipo="appendice"}else{$tipo=""}' \
@@ -35,6 +37,7 @@ if [ "$code" -eq 200 ]; then
   then put '$data=sub($url,"^(.+COVID-19_)(.+2020)(.+)$","\2")' \
   then cut -o -f nomefile,data,tipo,url "$folder"/processing/lista.csv >"$folder"/../output/"$nome".csv
 
+  # crea la versione markdown
   mlr --c2m put '$nomefile="[".$nomefile."](".$url.")"' then cut -x -f url "$folder"/../output/"$nome".csv >"$folder"/../output/"$nome".md
 
 fi
