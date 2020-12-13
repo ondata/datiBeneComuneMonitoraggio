2513e40432bd4a23619bede5a9f063cf57c162de
Andrea Borruso <aborruso@gmail.com>
2020-12-13 11:03:05 +0100

    close  #14

--- script/webarchive.sh
+++ script/webarchive.sh
@@ -15,10 +15,17 @@ yq <"$folder"/../risorse/webarchive.yml . | mlr --j2t cut -f URL,if_not_archived
 rm "$folder"/webarchiveLatest.log
 
 while IFS=$'\t' read -r url time; do
-  curl -X POST -H "Accept: application/json" -H "Authorization: LOW $SUPER_SECRET" -d 'url='"$url"'/&capture_outlinks=1&capture_screenshot=1&outlinks_availability=1&if_not_archived_within='"$time"'' https://web.archive.org/save >>"$folder"/webarchiveLatest.log
+  #curl -X POST -H "Accept: application/json" -H "Authorization: LOW $SUPER_SECRET" -d 'url='"$url"'/&capture_outlinks=1&capture_screenshot=1&outlinks_availability=1&if_not_archived_within='"$time"'' https://web.archive.org/save >>"$folder"/webarchiveLatest.log
+  #sleep 10
+  curl -X POST -H "Accept: application/json" -H "Authorization: LOW $SUPER_SECRET" \
+    -d "url=$url" \
+    -d "capture_outlinks=1" \
+    -d "capture_screenshot=1" \
+    -d "outlinks_availability=1" \
+    -d "if_not_archived_within=$time" https://web.archive.org/save >>"$folder"/webarchiveLatest.log
   sleep 10
 done <"$folder"/../risorse/webarchive.tsv
 
-<"$folder"/webarchiveLatest.log jq -c . >"$folder"/tmp.log
+jq <"$folder"/webarchiveLatest.log -c . >"$folder"/tmp.log
 
 mv "$folder"/tmp.log "$folder"/webarchiveLatest.log
