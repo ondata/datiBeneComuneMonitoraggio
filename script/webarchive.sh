08900a0590bcbbfccbd81c008fa1f4d6d28667b3
Andrea Borruso <aborruso@gmail.com>
2020-12-13 11:06:45 +0100

    pulizia

--- script/webarchive.sh
+++ script/webarchive.sh
@@ -15,8 +15,6 @@ yq <"$folder"/../risorse/webarchive.yml . | mlr --j2t cut -f URL,if_not_archived
 rm "$folder"/webarchiveLatest.log
 
 while IFS=$'\t' read -r url time; do
-  #curl -X POST -H "Accept: application/json" -H "Authorization: LOW $SUPER_SECRET" -d 'url='"$url"'/&capture_outlinks=1&capture_screenshot=1&outlinks_availability=1&if_not_archived_within='"$time"'' https://web.archive.org/save >>"$folder"/webarchiveLatest.log
-  #sleep 10
   curl -X POST -H "Accept: application/json" -H "Authorization: LOW $SUPER_SECRET" \
     -d "url=$url" \
     -d "capture_outlinks=1" \
