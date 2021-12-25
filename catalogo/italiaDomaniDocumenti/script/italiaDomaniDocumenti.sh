7eb3e3cae635681459f9a10f5a0a44107ff02ac6
Andrea Borruso <aborruso@gmail.com>
2021-12-25 18:11:46 +0100

    update

--- catalogo/italiaDomaniDocumenti/script/italiaDomaniDocumenti.sh
+++ catalogo/italiaDomaniDocumenti/script/italiaDomaniDocumenti.sh
@@ -15,6 +15,7 @@ mkdir -p "$folder"/../output
 
 URLBase="https://italiadomani.gov.it/it/strumenti/documenti/jcr:content/root/container/documentssearch.searchResults.html?orderby=%2540jcr%253Acontent%252Fdate&sort=desc"
 
+# controlla risposta pHTTP agina e scarica elenco dei documenti
 code=$(curl -s -L -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0' -o "$folder"/rawdata/tmp.html -w "%{http_code}" "$URLBase")
 
 # se il server risponde fai partire lo script
@@ -26,27 +27,33 @@ else
   exit 1
 fi
 
+# estrai tipo e autore documento
 scrape <"$folder"/rawdata/tmp.html -be "//div[@class='banner-documents']" | xq -c '.html.body.div[]|{type:.span[0]."#text",author:.span[1]."#text"}' | mlr --json cat -n >"$folder"/rawdata/tmpTypeAuthor.json
 
+# estrai data, titolo e path documento
 scrape <"$folder"/rawdata/tmp.html -be "//div[@data-cmp-hook-search='wrapper']" | xq -c '.html.body.div[]|{
       date:.div["@data-date"],
       title:.div.div[0].h4.button.span,
       path:.div.div[1].div.div[0].a["@href"]
       }' >"$folder"/rawdata/tmp_data.json
 
+# aggiungi id ai dati, utile per fare il join tra i file
 mlr -I --json cat -n "$folder"/rawdata/tmp_data.json
 
+# join tra dati con dati su tipo e autore
 mlr --json join --ul -j n -f "$folder"/rawdata/tmp_data.json then unsparsify then put -S '$datetime = strftime(strptime($date, "%d/%m/%y"),"%Y-%m-%d");$URL="https://italiadomani.gov.it".$path' then sort -r datetime -f title then clean-whitespace then reorder -f date,type,author,title,path,datetime,URL "$folder"/rawdata/tmpTypeAuthor.json >"$folder"/../output/latest.json
 
-# estrai tag
+# estrai tag dei documenti
 scrape <"$folder"/rawdata/tmp.html -be '//div[@class="banner-documents"]' | xq '.html.body.div[]' | mlr --json cat -n then unsparsify then cut -r -f '^(n|.*div.+text.*)$' then reshape -r "div" -o i,v then filter -x -S '$v==""' then cut -x -f i then nest --implode --values --across-records -f v then rename v,tag >"$folder"/rawdata/tmpTag.json
 
+# fai join tra i dati e dati con i tag
 mlr --json join --ul -j n -f "$folder"/../output/latest.json then unsparsify then cut -x -f n "$folder"/rawdata/tmpTag.json >"$folder"/rawdata/tmp_data.json
-
 mv "$folder"/rawdata/tmp_data.json "$folder"/../output/latest.json
 
+# aggiorna latest
 mlr --j2c cat "$folder"/../output/latest.json >"$folder"/../output/latest.csv
 
+# crea file di archivio e se c'è già aggiornalo
 if [ ! -f "$folder"/../output/archive.csv ]; then
   cp "$folder"/../output/latest.csv "$folder"/../output/archive.csv
 else
@@ -54,17 +61,18 @@ else
   mv "$folder"/rawdata/tmp.csv "$folder"/../output/archive.csv
 fi
 
-# RSS
-
+# genera RSS
 title="Italia Domani, Documenti | a cura di #datiBeneComune"
 description="Il feed RSS per essere aggiornati su nuove pubblicazioni nella sezione Documenti di Italia Domani"
 selflinkraw="https://ondata.github.io/datiBeneComuneMonitoraggio/catalogo/italiaDomaniDocumenti/rss.xml"
 
+# estrai dati per RSS
 mlr --csv put -S '$pubDate = strftime(strptime($datetime, "%Y-%m-%d"),"%Y-%m-%dT%H:%M:%SZ");$title="#".$type." | ".$title' then rename URL,link then cut -o -f title,link,pubDate "$folder"/../output/latest.csv >"$folder"/../output/rss.csv
 
+# crea feed RSS
 ogr2ogr -f geoRSS -dsco TITLE="$title" -dsco LINK="$selflinkraw" -dsco DESCRIPTION="$description" "$folder"/../rss.xml "$folder"/../output/rss.csv -oo AUTODETECT_TYPE=YES
 
-# open-data
+# scarica le risorse definite come open-data
 mkdir -p "$folder"/../output/open-data/rawdata
 cd "$folder"/../output/open-data/rawdata
 mlr --c2n filter -S 'tolower($tag)=~"open"' then cut -f URL "$folder"/../output/latest.csv | while read line; do
